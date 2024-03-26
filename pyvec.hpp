ba35a304adacc7277745f71327bd02a496a29fba
lyk <lyk-boya@outlook.com>
2024-03-26 21:37:04 +0800

    init py interface

--- pyvec.hpp
+++ pyvec.hpp
@@ -75,11 +75,36 @@ private:
         return *ans;
     }
 
+    struct slice_type {
+        size_type l, r;
+        size_type num_step;
+        difference_type step;
+        slice_type(const size_type l, const size_type r, const difference_type step): l(l), r(r), step(step) {
+            if(step == 0) { throw std::invalid_argument("pyvec: step == 0"); }
+            const difference_type n = static_cast<difference_type>(r - l) / step;
+            num_step = n > 0? n: 0;
+        }
+    };
+
 
     [[nodiscard]] size_type py_index(difference_type i) const {
         i = i < 0 ? size() + i : i;
         if (i < 0 | i >= size()) { throw std::out_of_range("pyvec: index out of range"); }
         return static_cast<size_type>(i);
+    }
+
+    [[nodiscard]] slice_type py_slice(const difference_type begin, const difference_type end, const difference_type step=1) const {
+        const size_type l = py_index(begin);
+        const size_type r = end == _ptrs.size()? end: py_index(end);
+        return slice_type{l, r, step};
+    }
+
+    void ref_other(const pyvec& other) {
+        _shared_resources.reserve(other._shared_resources.size() + 1 + _shared_resources.size());
+        _shared_resources.push_back(other._resources);
+        for(const auto &res: other._shared_resources) {
+            _shared_resources.push_back(res);
+        }
     }
 
 public:
@@ -363,19 +388,14 @@ public:
     }
 
     // python like slice, using shallow copy
-    pyvec<T> slice(const difference_type begin, const difference_type end) const {
-        const size_type l = py_index(begin);
-        const size_type r = end == _ptrs.size()? end: py_index(end);
-        if(l > r) { throw std::invalid_argument("pyvec: begin > end"); }
-
+    pyvec<T> slice(const difference_type begin, const difference_type end, const difference_type step=1) const {
+        const auto t_slice = py_slice(begin, end, step);
+        if(t_slice.num_step <= 0) { return pyvec<T>{}; }
         pyvec ans{};
-        ans._shared_resources.resize(_shared_resources.size() + 1);
-        for(auto i = 0; i < _shared_resources.size(); ++i) {
-            ans._shared_resources[i] = _shared_resources[i];
-        }   ans._shared_resources.back() = _resources;
-        ans._ptrs.resize(r - l);
-        for(auto i = l; i < r; ++i) {
-            ans._ptrs[i - l] = _ptrs[i];
+        ans.ref_other(*this);
+        ans._ptrs.reserve(t_slice.num_step);
+        for(auto i = t_slice.l; i < t_slice.r; i += step) {
+            ans._ptrs.push_back(_ptrs[i]);
         }
         return ans;
     }
@@ -633,6 +653,67 @@ public:
     [[nodiscard]] bool operator>(const pyvec<T>& other) const { return other < *this; }
     [[nodiscard]] bool operator<=(const pyvec<T>& other) const { return !(other < *this); }
     [[nodiscard]] bool operator>=(const pyvec<T>& other) const { return !(*this < other); }
+    /*
+     *  Python like iterface
+     */
+    void append(const_reference value) { push_back(value); }
+    void append(T&& value) { push_back(std::move(value)); }
+
+    void extend(const pyvec& other) {
+        ref_other(other);
+        _ptrs.reserve(_ptrs.size() + other.size());
+        for(const auto& ptr: other._ptrs) {
+            _ptrs.push_back(ptr);
+        }
+    }
+
+    shared<T> getitem(const difference_type i) { return share(i); }
+
+    pyvec<T> getitem(const difference_type begin, const difference_type end, const difference_type step=1) const {
+        return slice(begin, end, step);
+    }
+
+    void delitem(const difference_type i) { erase(cbegin() + py_index(i)); }
+
+    void delitem(const difference_type begin, const difference_type end, const difference_type step=1) {
+        const auto t_slice = py_slice(begin, end, step);
+        // using filter
+        if(t_slice.num_step <= 0) { return; }
+        std::vector<pointer> new_ptrs;
+        new_ptrs.reserve(size() - t_slice.num_step);
+        for(auto i = 0; i < size(); ++i) {
+            if(i < t_slice.l | i >= t_slice.r | (i - t_slice.l) % t_slice.step != 0) {
+                new_ptrs.push_back(_ptrs[i]);
+            }
+        }
+        _ptrs = std::move(new_ptrs);
+    }
+
+    size_type index(const_reference value) const {
+        for(auto i = 0; i < size(); ++i) {
+            if(*_ptrs[i] == value) { return i; }
+        }   throw std::runtime_error("pyvec::index: value is not in the list");
+    }
+
+    size_type count(const_reference value) const {
+        size_type ans = 0;
+        for(const auto& ptr: _ptrs) {
+            if(*ptr == value) { ++ans; }
+        }   return ans;
+    }
+
+    void insert(difference_type pos, const_reference value) { insert(cbegin() + pos, value); }
+
+    shared<T> pop() {
+        auto ans = share(size() - 1);
+        pop_back();
+        return ans;
+    }
+
+    void reverse() {
+        std::reverse(_ptrs.begin(), _ptrs.end());
+    }
+
 };
 
 #endif // PY_VEC_HPP_
