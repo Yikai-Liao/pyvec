50b6a6767f44d574292f0b5a147e8a56651dd0b5
Yikai Liao <lyk-boya@outlook.com>
2024-04-11 12:55:44 +0800

    add shared ptr to benchmark

--- bench.cpp
+++ bench.cpp
@@ -11,20 +11,45 @@
 using namespace ankerl;
 
 int main() {
-    std::cout << std::endl << "vector" << std::endl;
-    nanobench::Bench().minEpochIterations(10000).run("push_back", [&]() {
-        std::vector<int> v;
-        v.reserve(10000);
-        for (int i = 0; i < 1000; ++i) { v.push_back(i); }
-        nanobench::doNotOptimizeAway(v);
-    });
+    const size_t num = 10000;
+    nanobench::Bench()
+        .minEpochIterations(1000)
+        .run(
+            "vector::push_back",
+            [&]() {
+                std::vector<int> v;
+                v.reserve(num);
+                for (int i = 0; i < num; ++i) { v.push_back(i); }
+                nanobench::doNotOptimizeAway(v);
+            }
+        )
+        .run(
+            "pyvec::push_back",
+            [&]() {
+                pyvec<int> v;
+                v.reserve(num);
+                for (int i = 0; i < num; ++i) { v.push_back(i); }
+                nanobench::doNotOptimizeAway(v);
+            }
+        )
+        .run(
+            "shared_vec::push_back",
+            [&]() {
+                std::vector<std::shared_ptr<int>> v;
+                v.reserve(num);
 
-    std::cout << std::endl << "pyvec" << std::endl;
+                auto capsule = std::make_shared<std::vector<int>>();
+                capsule->reserve(num);
 
-    nanobench::Bench().minEpochIterations(10000).run("push_back", [&]() {
-        pyvec<int> v;
-        v.reserve(10000);
-        for (int i = 0; i < 1000; ++i) { v.push_back(i); }
-        nanobench::doNotOptimizeAway(v);
-    });
+                for (int i = 0; i < num; ++i) {
+                    capsule->push_back(i);
+                    v.emplace_back(capsule, &capsule->back());
+                }
+                nanobench::doNotOptimizeAway(v);
+            }
+        )
+
+        // clang-format off
+    ;
+    // clang-format on
 }
