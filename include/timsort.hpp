347e8796950b8c5f57d63dc137a157b4ecdba896
Yikai Liao <lyk-boya@outlook.com>
2024-06-12 17:35:20 +0800

    timsortv2

--- include/timsort.hpp
+++ include/timsort.hpp
@@ -3,11 +3,13 @@
 #define GFX_TIMSORT_WRAPPER
 
 // clang-format off
-#if __has_include(<ranges> )
-  #include "gfx/timsort3.hpp"
-#else
-  #include "gfx/timsort2.hpp"
-#endif
+// #if __has_include(<ranges> )
+//   #include "gfx/timsort3.hpp"
+// #else
+//   #include "gfx/timsort2.hpp"
+// #endif
+// Apple Clang 14 arm failed to check ranges
+#include "gfx/timsort2.hpp"
 // clang-format on
 
 #endif   // GFX_TIMSORT_WRAPPER
